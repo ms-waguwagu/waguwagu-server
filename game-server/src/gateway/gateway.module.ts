4f082fd381516456dac0e961429fac77c0677f8c
Zuzzang2 <mandy021021@gmail.com>
2025-12-11 09:37:15 +0900

    fix: 파일 경로 수정

--- game-server/src/gateway/gateway.module.ts
+++ game-server/src/gateway/gateway.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
 import { RankingModule } from '../ranking/ranking.module';
-import { GhostModule } from "../engine/ghost/ghost.module"
+import { GhostModule } from '../engine/ghost/ghost.module';
 
 @Module({
   imports: [RankingModule, GhostModule],
--- matching-server/src/main.ts
+++ matching-server/src/main.ts
@@ -3,13 +3,18 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { Logger } from '@nestjs/common';
+
 
 
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {logger: ['log', 'error', 'warn', 'debug', 'verbose'], // 모든 로그 활성화
+  });
   const configService = app.get(ConfigService);
+
+	
 
 	app.useGlobalPipes(
 		new ValidationPipe({
