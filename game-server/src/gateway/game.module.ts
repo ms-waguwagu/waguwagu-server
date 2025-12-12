b508002a293ce45c19d5b1322eaef67bd21a9303
Zuzzang2 <mandy021021@gmail.com>
2025-12-12 10:18:53 +0900

    fix: 프론트에서 방 번호 하드코딩된 부분 수정

--- game-server/src/gateway/game.module.ts
+++ game-server/src/gateway/game.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
 import { RankingModule } from '../ranking/ranking.module';
 import { GhostModule } from '../engine/ghost/ghost.module';
+import { GameController } from './game.controller';
 
 @Module({
   imports: [RankingModule, GhostModule],
+  controllers: [GameController],
   providers: [GameGateway],
   exports: [GameGateway],
 })
