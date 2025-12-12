cfc65ab2dc403391bd615754cc800b029d8007c3
Zuzzang2 <mandy021021@gmail.com>
2025-12-12 16:55:30 +0900

    chore: 안쓰는 컨트롤러 정리

--- matching-server/src/queue/queue.module.ts
+++ matching-server/src/queue/queue.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QueueGateway } from './queue.gateway';
-import { QueueController } from './queue.controller';
 import { QueueService } from './queue.service';
 import { AuthModule } from '../auth/auth.module';
 
 
 @Module({
 	imports: [AuthModule],
-	controllers: [QueueController],
 	providers: [QueueGateway, QueueService],
 	exports: [QueueService, QueueGateway],
 })
