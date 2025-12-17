5b4dc91fc29e693ed5fbcc22ea770a66e1d7ecd6
kwakmh <kmh001114@naver.com>
2025-12-17 10:20:21 +0900

    feat:oidc, helm 구조 분리

---
+++ terraform/matching/helm-alb-controller.tf
@@ -0,0 +1,34 @@
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.alb_controller_policy_attach,
+    kubernetes_service_account.alb_sa
+  ]
+
+  set {
+    name  = "clusterName"
+    value = local.cluster_name
+  }
+  set {
+    name  = "region"
+    value = var.region
+  }
+  set {
+    name  = "vpcId"
+    value = data.aws_eks_cluster.this.vpc_config[0].vpc_id
+  }
+
+  # SA는 우리가 만들었으니 chart에서는 생성하지 않게
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
--- terraform/matching/irsa-alb-controller.tf
+++ terraform/matching/irsa-alb-controller.tf
@@ -1,21 +1,3 @@
-locals {
-  cluster_name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster" "this" {
-  name = local.cluster_name
-}
-
-data "tls_certificate" "eks" {
-  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "eks" {
-  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-}
-
 data "aws_iam_policy_document" "alb_controller_assume" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -63,40 +45,5 @@ resource "kubernetes_service_account" "alb_sa" {
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller_role.arn
     }
-  }
-}
-
-resource "helm_release" "alb_controller" {
-  name       = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-
-  depends_on = [
-    aws_iam_role_policy_attachment.alb_controller_policy_attach,
-    kubernetes_service_account.alb_sa
-  ]
-
-  set {
-    name  = "clusterName"
-    value = local.cluster_name
-  }
-  set {
-    name  = "region"
-    value = var.region
-  }
-  set {
-    name  = "vpcId"
-    value = data.aws_eks_cluster.this.vpc_config[0].vpc_id
-  }
-
-  # SA는 우리가 만들었으니 chart에서는 생성하지 않게
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
   }
 }
