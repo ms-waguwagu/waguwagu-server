4338db266767bc7cc61c2943f5b3291ef60cfc78
Zuzzang2 <mandy021021@gmail.com>
2025-12-21 23:22:50 +0900

    feat: S3 및 Cloudfront + ALB DNS 자동화 스크립트 추가

---
+++ cloudformation/deploy-cloudfront.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+MATCHING_CTX="arn:aws:eks:ap-northeast-2:796973515852:cluster/T3-Wagu-Matching-EKS"
+
+echo "▶ 매칭 클러스터로 전환"
+kubectl config use-context "$MATCHING_CTX"
+
+echo "▶ matching-ingress ALB 대기"
+kubectl wait --for=jsonpath='{.status.loadBalancer.ingress[0].hostname}' \
+  ingress/matching-ingress -n matching --timeout=10m
+
+MATCHING_DNS=$(kubectl get ingress matching-ingress -n matching \
+  -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+
+echo "Matching ALB: $MATCHING_DNS"
+
+echo "▶ CloudFront 배포"
+aws cloudformation deploy \
+  --template-file T3-Wagu-Cloudfront.yaml \
+  --stack-name T3-Wagu-Cloudfront \
+  --parameter-overrides \
+    MatchingAlbDns=$MATCHING_DNS \
+  --capabilities CAPABILITY_NAMED_IAM
