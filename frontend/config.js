0f5fecbe0d713c6e8f0e233d6f419b5d33d58092
MinJ-i <smg9926@naver.com>
2025-12-29 15:37:50 +0900

    fix: config.js 경로명 수정

--- frontend/config.js
+++ frontend/config.js
@@ -1,16 +1,14 @@
-export const CONFIG = {
-  API_URL: "https://matching.waguwagu.cloud/api/game",
-  SOCKET_URL: "https://matching.waguwagu.cloud/api/game/game",
-  BOSS_GAME_URL: "https://matching.waguwagu.cloud/api/game/boss",
-};
-
-
 export const MATCHING_CONFIG = {
-	API_BASE_URL: "https://matching.waguwagu.cloud",
+  API_BASE_URL: "https://matching.waguwagu.cloud",
+  WS_BASE_URL: "wss://matching.waguwagu.cloud",
 
-  // WebSocket (ALB 직결)
-  WS_MATCHING_URL: "https://matching.waguwagu.cloud/queue",
-	
   GOOGLE_CLIENT_ID:
     "154265017834-ekukrom3qijqqks90uetmptji64bhto4.apps.googleusercontent.com",
+};
+
+export const CONFIG = {
+  API_URL: `${MATCHING_CONFIG.API_BASE_URL}/api/game`,
+  SOCKET_URL: `${MATCHING_CONFIG.WS_BASE_URL}/api/game/game`,
+  BOSS_GAME_URL: `${MATCHING_CONFIG.API_BASE_URL}/api/game/boss`,
+  WS_MATCHING_URL: `${MATCHING_CONFIG.WS_BASE_URL}/queue`,
 };
